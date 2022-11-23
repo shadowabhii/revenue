8dd04607fff886e8b41f439fbfef54cab9bed620
Abhishek Yadav <abhishek44221@gmail.com>
2022-11-23 12:21:04 +0530

    Test case

--- src/test/java/com/revenue/AuthorRevenueApplicationTests.java
+++ src/test/java/com/revenue/AuthorRevenueApplicationTests.java
@@ -1,7 +1,12 @@
 package com.revenue;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.sql.Date;
 
@@ -9,16 +14,27 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.netflix.discovery.converters.Auto;
 import com.revenue.model.Transaction;
+import com.revenue.service.FinancialCustomRepository;
+import com.revenue.service.IFinancialsRepository;
+
+
 
 @SpringBootTest(classes = AuthorRevenueApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
 //@SpringBootTest
+@AutoConfigureMockMvc
 class AuthorRevenueApplicationTests {
 	
 	  
@@ -29,6 +45,17 @@ class AuthorRevenueApplicationTests {
 	  
 	  private String baseUrl="http://localhost";
 	  
+	  @Autowired
+	  private MockMvc mockMvc;
+	  
+	  @Autowired
+	 private FinancialCustomRepository financialCustomRepository;
+	  
+	  @Autowired
+	 private  IFinancialsRepository financialRepo;
+	  
+	  @Autowired
+	  private ObjectMapper objectMapper;
 	  
 //	  @Autowired
 //	  private static TestRestTemplate restTemplate;
@@ -40,6 +67,7 @@ class AuthorRevenueApplicationTests {
 	  @Autowired 
 	  private ITestH2Repository testRepo;
 	  
+	  
 	  @BeforeAll public static void init() { restTemplate=new RestTemplate(); }
 	  
 	  @BeforeEach public void setUp() {
@@ -48,13 +76,27 @@ class AuthorRevenueApplicationTests {
 	  }
 	  
 	  
-	  @Test public void testTransaction() { 
+//	  @Test public void testTransaction() { 
+//		  Transaction transaction = new	  Transaction(24,"kevin",100.0,null,1,true,3,2,4,"cartoon");
+//	  
+//	  Transaction response = restTemplate.postForObject(baseUrl+"/transactionss", transaction, Transaction.class);
+//	  
+//	  assertEquals("catoon", response.getBookTitle()); 
+//	  assertEquals(1, testRepo.findAll().size()); 
+//	  }
+	  
+	  @Test
+	  public void testTransaction() throws Exception
+	  {
 		  Transaction transaction = new	  Transaction(24,"kevin",100.0,null,1,true,3,2,4,"cartoon");
-	  
-	  Transaction response = restTemplate.postForObject(baseUrl+"/transactionss", transaction, Transaction.class);
-	  
-	  assertEquals("catoon", response.getBookTitle()); 
-	  assertEquals(1, testRepo.findAll().size()); 
+		  
+		  ResultActions response = mockMvc.perform(post("/transactionss")
+				  .contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(transaction)));
+				  
+			response.andDo(print()).
+			andExpect(status().isCreated())
+			.andExpect(jsonPath("$.readerName", is(transaction.getReaderName())));
+				  
 	  }
 	  
 	  @Test public void testRevenue() { 
@@ -68,6 +110,19 @@ class AuthorRevenueApplicationTests {
 	 // assertEquals(1, testRepo.findAll().size()); 
 	  }
 	  
+	  
+	  @Test public void testRevenueBetweenDates() { 
+		  Double price=0.0;
+		  
+	 Date startDate = null;
+	 Date endDate = null;
+	 int authorId=1;
+	  
+	  Double response = restTemplate.getForObject(baseUrl+"rev/"+ startDate +"/"+endDate+authorId, Double.class);
+	  
+	  assertEquals(136, response); 
+	 // assertEquals(1, testRepo.findAll().size()); 
+	  }
 	  
 	  
 	  
